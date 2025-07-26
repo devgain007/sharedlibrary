c93336c2532434afcf8bdf3e176c56fabef0972c
devgain007 <devgain1234@gmail.com>
2025-07-26 16:18:45 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,7 +6,7 @@ def buildArtifact()
 {
   sh 'python3'
 }
-def deployment(jobname,ip,context)
+def deployment(jobname,ip)
 {
     sh "scp '/var/lib/jenkins/workspace/${jobname}/prac.py' ubuntu@${ip}:/etc/prac.py"
 
