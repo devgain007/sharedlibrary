b877aafeccc7dd727c7522537155aea7f05853f3
devgain007 <devgain1234@gmail.com>
2025-07-26 13:53:12 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,4 +1,4 @@
 def Gitdownload(repo)
 {
-    git branch: 'main', url: "https://github.com/devgain007/project-devops//${repo}.git"
+    git branch: 'main', url: "https://github.com/devgain007/project-devops/${repo}.git"
 }
